1dabca70290abd7fc07cd70aee17be05b2c0a615
Rodrigo Bornia <rodrigobornia@gmail.com>
2026-06-28 19:01:02 -0300

    Agrega metodo a sucursal

---
+++ src/envio/Sucursal.java
@@ -0,0 +1,12 @@
+package envio;
+
+import pedido.Pedido;
+
+public class Sucursal {
+
+	public boolean tieneStockDePedido(Pedido pedido) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
